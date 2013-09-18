5f4049c7c7b932e8a9c90267275114b61f419477
Pablo Marcondes Fonseca <pablo.mmarcondes@gmail.com>
2013-09-18 11:56:27 -0300

    retirando codigo de teste

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "BibtexArticle.h"
 #include "BibtexBook.h"
 #include "BibtexInproceedings.h"
+#include "bibfileio.h"
 
 using namespace std;
 
@@ -16,36 +17,8 @@ int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
-    cout << Util::parseInt("{123}") << endl;
-    cout << Util::parseString("{456}") << endl;
-    cout << Util::parseString("abcd") << endl;
-    cout << Util::parseString("{efgh}") << endl;
-    cout << Util::parseField("Title","Title = \"Something Great\",") << endl;
-    cout << Util::parseField("Title","Title = {Something Great2},") << endl;
-    AuthorReader::parseAuthors("author = \"Joe Torre and Tom Verducci\",");
-    PagesReader::parsePages("author = \"Joe Torre and Tom Verducci\",pages = {1,2,3}");
-    PagesReader::parsePages("pages = {1-5,8,9,20-22}");
-
-    BibtexArticle* book = new BibtexArticle();
-    book->setReference("myref");
-    book->setTitle("mytitle");
-    book->setYear(2013);
-    Authors authors;
-    authors.addAuthor("autor1");
-    authors.addAuthor("autor2");
-    book->setAuthors(authors);
-    book->setJournal("jornal1");
-    book->setNumber(1);
-    book->setVolume(2);
-    Pages pages;
-    pages.addPage(12);
-    pages.addPage(13);
-    book->setPages(pages);
-    BibFile bibFile;
-    bibFile.createBibtex(book);
-    cout << bibFile.toText() << endl;
-
-    Util::test();
+    BibfileIO fileio("file.bib");
+    cout << "fileio=\n" << fileio.getBibFile()->toText() << endl;
 
     QtQuick2ApplicationViewer viewer;
     viewer.setMainQmlFile(QStringLiteral("qml/tp1/main.qml"));
